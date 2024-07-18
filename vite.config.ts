352c0116798f9bb3635a2b8d68cb69448fa7606b
heywon0909 <hw7319@naver.com>
2024-07-18 19:36:40 +0900

    feat: vite base 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/introduce-me/',
+    build: {
+        rollupOptions: {
+            input: 'index.html',
+            output: {
+                dir: 'build',
+            },
+        },
+    },
     plugins: [react()],
     resolve: {
         alias: [
