f0e70680acfed320e6b0daf5ec87895230f827ca
@hyewon_park2 <heywon_park2@tmax.co.kr>
2024-07-19 09:42:06 +0900

    feat: assetfilenames 파일 확장자명에 맞게 수정

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
             output: {
                 dir: 'build',
                 entryFileNames: '[name]-[hash].js',
-                assetFileNames: '[name]-[hash].js',
+                assetFileNames: '[name]-[hash][extname]',
             },
         },
     },
