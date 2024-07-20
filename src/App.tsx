194e87e09fc33050cfedf625e4bab5fa778275e0
heywon0909 <hw7319@naver.com>
2024-07-20 11:06:55 +0900

    feat: react query library setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import './App.css'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { Routes } from './router/routes'
 
+
+// Create a client
+const queryClient = new QueryClient()
+
 function App() {
-    return <Routes />
+    return (
+        // Provide the client to your App
+        <QueryClientProvider client={queryClient}>
+        <Routes />
+        </QueryClientProvider>
+    )
 }
 
 export default App
