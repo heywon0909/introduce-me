a872c0fc57135f0f2d1adfda1abde3cf2686f395
@hyewon_park2 <heywon_park2@tmax.co.kr>
2024-07-24 12:45:34 +0900

    feat: reset css 추출

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
-import * as reset from './reset.css'
-import { Global } from '@emotion/react'
+// import * as reset from './reset.css'
+// import { Global } from '@emotion/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Routes } from './router/routes'
 
@@ -11,7 +11,7 @@ function App() {
     return (
         // Provide the client to your App
         <QueryClientProvider client={queryClient}>
-            <Global styles={reset} />
+            {/* <Global styles={reset} /> */}
             <Routes />
         </QueryClientProvider>
     )
