e1b5b38bf5de44f5259ea5f7d9a3a986bcd91c62
heywon0909 <hw731911@gmail.com>
2024-08-03 12:16:39 +0900

    feat: bunney loading component add

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,10 +1,11 @@
 import Box from '@mui/material/Box'
-import CircularProgress from '@mui/material/CircularProgress'
+import { BunneyLoading } from 'bunney-ui/BunneyLoading'
 
 export const Loading = () => {
+
     return (
          <Box sx={{ display: 'flex',width:'auto',height:'87vh',justifyContent:'center',alignItems:'center' }}>
-            <CircularProgress />
+            <BunneyLoading />
         </Box>
      )
 }
