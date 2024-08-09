64d98083ea765d3669a61aebe73b35585c1588c1
@hyewon_park2 <heywon_park2@tmax.co.kr>
2024-08-09 18:13:00 +0900

    feat: useDelayedQuery hook eslint add

--- src/hooks/useDelayedQuery.tsx
+++ src/hooks/useDelayedQuery.tsx
@@ -1,10 +1,18 @@
-import { QueryKey, useQuery } from '@tanstack/react-query';
-import {fetchingDelay } from '@utils/fetchingDelay';
+import { QueryKey, useQuery } from '@tanstack/react-query'
+import { fetchingDelay } from '@utils/fetchingDelay'
 
-export const useDelayedQuery = <TQueryFnData=unknown,TQueryKey extends QueryKey = QueryKey>({queryKey,queryFn}:{ queryKey: TQueryKey, queryFn:Promise<TQueryFnData>}) => {
+export const useDelayedQuery = <
+    TQueryFnData = unknown,
+    TQueryKey extends QueryKey = QueryKey
+>({
+    queryKey,
+    queryFn,
+}: {
+    queryKey: TQueryKey
+    queryFn: Promise<TQueryFnData>
+}) => {
     return useQuery({
         queryKey,
-        queryFn: ()=> fetchingDelay(queryFn) as TQueryFnData
+        queryFn: () => fetchingDelay(queryFn) as TQueryFnData,
     })
 }
-
