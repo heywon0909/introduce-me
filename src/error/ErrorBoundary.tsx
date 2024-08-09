8d74beff3c4e33a0d64e7d4e1f08e39d835b5362
heywon0909 <hw731911@gmail.com>
2024-08-09 21:24:21 +0900

    feat: error boundary class fallback ui add

--- src/error/ErrorBoundary.tsx
+++ src/error/ErrorBoundary.tsx
@@ -2,12 +2,30 @@ import { Component } from 'react'
 
 interface State {
     hasError: boolean
-    info?: Error
+    info?: Error | null
 }
+export type ErrorFallbackProps<ErrorType extends Error = Error> = {
+    error: ErrorType
+    reset: (...args: unknown[]) => void
+}
+export type ErrorFallbackComponent = <ErrorType extends Error>(
+    props: ErrorFallbackProps<ErrorType>
+) => JSX.Element
 interface Props {
+    errorFallback: ErrorFallbackComponent
     children?: React.ReactNode
+    resetQuery?: () => void
+    keys?: unknown[]
 }
-const initialState: State = { hasError: false }
+const initialState: State = { hasError: false, info: null }
+
+const compareArr = (prevArr: Array<unknown>, nextArr: Array<unknown>) => {
+    return (
+        prevArr.length === nextArr.length &&
+        prevArr.some((item, index) => !Object.is(item, nextArr[index]))
+    )
+}
+
 export class ErrorBoundary extends Component<Props, State> {
     constructor(props: Props) {
         super(props)
@@ -21,16 +39,32 @@ export class ErrorBoundary extends Component<Props, State> {
     reset() {
         this.setState(initialState)
     }
+    resetBoundary = () => {
+        const { resetQuery } = this.props
+        resetQuery?.()
+        this.reset()
+    }
 
-    componentDidCatch(error: Error, errorInfo) {
-        // 에러 로깅 등의 작업을 수행할 수 있습니다.
-        console.log(error, errorInfo)
+    componentDidUpdate(prevProps: Props, prevState: State) {
+        const { info } = this.state
+        const { keys } = this.props
+
+        if (
+            info !== null &&
+            prevState.info !== null &&
+            prevProps.keys &&
+            compareArr(prevProps.keys, keys as unknown[])
+        ) {
+            this.resetBoundary()
+        }
     }
 
     render() {
-        if (this.state.hasError) {
-            return <h1>문제가 발생했습니다.</h1>
-        }
+        const { hasError, info } = this.state
+        const isError = hasError && info !== null
+        const fallbackErrorUI = (err: ErrorFallbackProps['error']) =>
+            this.props.errorFallback({ error: err, reset: this.resetBoundary })
+        if (isError) return fallbackErrorUI(info as Error)
 
         return this.props.children
     }
