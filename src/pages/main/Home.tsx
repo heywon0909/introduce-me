e1205c41dcf8b3beec12d8f65e3d8469bef22d75
heywon0909 <hw731911@gmail.com>
2024-11-18 17:22:30 +0900

    feat: react ui library 버벅임으로 인한 suspense 적용

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -2,8 +2,17 @@ import * as S from './styled'
 import { FrameText } from '@components/design/FrameText'
 import { ImgFrame } from '@components/design/MainImgFrame/ImgFrame'
 import { ImgSmallFrame } from '@components/design/MainImgFrame/atoms/ImgSmallFrame'
-import { ProfileImg } from '@components/design/MainImgFrame/atoms/ProfileImg'
+// import { ProfileImg } from '@components/design/MainImgFrame/atoms/ProfileImg'
 import { ContentWrapper } from '@components/common/Wrapper/ContentWrapper'
+import { lazy, Suspense } from 'react'
+
+const ProfileImg = lazy(() =>
+    import('@components/design/MainImgFrame/atoms/ProfileImg').then(
+        ({ ProfileImg }) => ({
+            default: ProfileImg,
+        })
+    )
+)
 
 export default function Home() {
     return (
@@ -12,7 +21,9 @@ export default function Home() {
             <S.Wrapper>
                 <ImgFrame>
                     <ImgSmallFrame>
-                        <ProfileImg />
+                        <Suspense fallback={<div></div>}>
+                            <ProfileImg />
+                        </Suspense>
                     </ImgSmallFrame>
                 </ImgFrame>
             </S.Wrapper>
