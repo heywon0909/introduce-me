25ea7b04bf2a318e53bf72f4a8f449a107b55e0d
heywon0909 <hw731911@gmail.com>
2024-11-19 18:41:03 +0900

    feat: project url 미구현으로 제거

--- src/pages/project/Project.tsx
+++ src/pages/project/Project.tsx
@@ -1,3 +1,10 @@
+import { CookieBunney } from 'bunney-ui/CookieBunney'
+
 export default function Project() {
-    return <div className="wrapper">test 중입니다...</div>
+    return (
+        <div className="wrapper">
+            <CookieBunney />
+            test 중입니다...
+        </div>
+    )
 }
