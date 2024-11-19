55e7c7b69a1590453792e07b44acd1d49261d4a8
heywon0909 <hw731911@gmail.com>
2024-11-19 18:42:28 +0900

    Updates

--- assets/Project-BfCKqxVq.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"../index-BMNiVPE3.js";function e(){return r.jsx("div",{className:"wrapper",children:"test 중입니다..."})}export{e as default};
---
+++ assets/Project-qqsJlZ5q.js
@@ -0,0 +1,6 @@
+import{n as a,K as p,M as r,N as x,j as e}from"../index-wQFacx-O.js";const s=a(p)`
+    justify-content: center;
+    align-items: center;
+    gap: 0;
+    position: relative;
+`,t=()=>r.jsx(s,{children:r.jsx(x,{color:"linear-gradient(#bf8055, #ffd1b2)",earColor:"linear-gradient(#67320e, #ffd1b2)",eyeColor:"#171717",styles:{boxShadow:"rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",border:"1px solid #daa37d"}})});function o(){return e.jsxs("div",{className:"wrapper",children:[e.jsx(t,{}),"test 중입니다..."]})}export{o as default};
