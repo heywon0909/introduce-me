c779f6a226014bf4f781042083e8576b9eb52209
heywon0909 <hw731911@gmail.com>
2026-07-16 15:38:07 +0900

    refactor: career scroll view 성능 개선

--- src/components/page/career/CareerDescription.tsx
+++ src/components/page/career/CareerDescription.tsx
@@ -1,34 +1,57 @@
+import gsap from 'gsap'
 import * as S from '@pages/career/styled'
 import FloatingBubbles from './FloatingBubble'
+import { forwardRef, useEffect, useRef } from 'react'
+import { ScrollTrigger } from 'gsap/all'
 
 interface Props {
+    id: string
     bg: string
     company: string
     description: string
     details: string[]
 }
-export const CareerDescription = ({
-    bg,
-    company,
-    description,
-    details,
-}: Props) => {
-    return (
-        <S.SectionBG bg={bg}>
-            <FloatingBubbles />
-            <S.Article>
-                <S.Company>
-                    <S.Title>{company}</S.Title>
-                    <S.Text>{description}</S.Text>
-                </S.Company>
-                <S.Description>
-                    <S.Ul>
-                        {details.map((detail) => (
-                            <S.Li>{detail}</S.Li>
-                        ))}
-                    </S.Ul>
-                </S.Description>
-            </S.Article>
-        </S.SectionBG>
-    )
-}
+
+gsap.registerPlugin(ScrollTrigger)
+
+export const CareerDescription = forwardRef<HTMLDivElement, Props>(
+    ({ id, bg, company, description, details }, ref) => {
+        const descriptionRef = useRef(null)
+
+        useEffect(() => {
+            gsap.to(descriptionRef.current, {
+                scrollTrigger: {
+                    trigger: descriptionRef.current,
+                    start: '20px 80%',
+                    end: 'bottom 100px',
+                    scrub: true,
+                    toggleActions: 'play none none reset',
+                },
+                yPercent: 0.5,
+                ease: 'elastic',
+                paused: true,
+                opacity: 1,
+                duration: 3,
+            })
+        }, [])
+
+        return (
+            <S.SectionBG bg={bg} ref={ref} id={id}>
+                <FloatingBubbles />
+                <S.Article>
+                    <S.Company>
+                        <S.Title>{company}</S.Title>
+                        <S.Text>{description}</S.Text>
+                    </S.Company>
+                    <S.Description data-animate ref={descriptionRef}>
+                        <S.Ul>
+                            {details.map((detail, i) => (
+                                <S.Li key={i}>{detail}</S.Li>
+                            ))}
+                        </S.Ul>
+                    </S.Description>
+                </S.Article>
+            </S.SectionBG>
+        )
+    }
+)
